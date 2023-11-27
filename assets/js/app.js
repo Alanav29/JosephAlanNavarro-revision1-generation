77e4255e72de9203310df2acafb0ecae400b38ef
Alan Navarro <josephalannavarro@gmail.com>
2023-11-27 14:41:13 -0600

    Se corigio index y app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,51 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+// cambio nombre constante de $n a nameRef
+// agrego "#" al querySelector de nameRef
+const nameRef = document.querySelector("#name");
+
+// cambio nombre constante de $b a blogRef
+// cambio a una section en el html con el id blog
+const blogRef = document.querySelector("#blog");
+
+// cambio nombre constante de $l a locationRef
+// creo una etiqueta h2 en el html con el id location
+const locationRef = document.querySelector("#location");
+
+/* Actualizo nombre de variables y cambio el tipo de función 
+displayUser a una asíncrona para permitir el uso de await*/
+async function displayUser(username) {
+	// Como el atributo textContent no existe cambio por innerHTML
+	nameRef.innerHTML = "cargando...";
+
+	/* Coloco response y las acciones posteriores
+   dentro de un bloque tryCatch para ejecutar acciones en caso de error */
+	try {
+		const response = await fetch(`${usersEndpoint}/${username}`);
+		// agrego la constante data que guardara los datos de usuario y los convertirá a un objeto
+		const data = await response.json();
+		console.log(data);
+		/* Como el atributo textContent no existe cambio por innerHTML
+    Cambio comillas dobles a template literals */
+		nameRef.innerHTML = `${data.name}`;
+		blogRef.innerHTML = `${data.blog}`;
+		locationRef.innerHTML = `${data.location}`;
+	} catch (error) {
+		// coloco acciones de la función handleError para manejar error desde el bloque catch
+		console.log("OH NO!");
+		console.log(error);
+		// actualizo a nombre correcto de variable nameRef
+		nameRef.textContent = `Algo salió mal: ${err}`;
+	}
 }
 
+// No elimino handleError por si es de utilidad en otra parte del código
 function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+	console.log(err);
+	console.log("OH NO!");
+	n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+// Ejecuto displayUser quitando el catch que ahora es innecesario
+displayUser("stolinski");
